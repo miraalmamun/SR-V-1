7e73dfaa35ef8cf513d5bbd3805b66c8031f11e1
Mir Al Mamun <miraalmamun@gmail.com>
2023-11-04 23:49:56 -0400

    Refactors 4

--- src/test/java/org/mir/TestCaseOne.java
+++ src/test/java/org/mir/TestCaseOne.java
@@ -20,7 +20,7 @@ public class TestCaseOne {
         // Initialize the ChromeDriver with the given options
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://google.com");
-        System.out.println("Title: " + driver.getTitle()); // Print the page title
+        System.out.println("***************************Title: " + driver.getTitle()); // Print the page title
         driver.quit();
     }
 }
